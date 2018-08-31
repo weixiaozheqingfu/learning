6833126865950b3c977f1097d951354c4b365833
李孟军 <limengjun@syswin.com>
2018-08-31 15:45:52 +0800

    提交修改

--- Java/spring-boot/1.spring boot 搭建web服务/spring-boot-web/src/main/java/com/glitter/spring/boot/Application.java
+++ Java/spring-boot/1.spring boot 搭建web服务/spring-boot-web/src/main/java/com/glitter/spring/boot/Application.java
@@ -3,8 +3,10 @@ package com.glitter.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.glitter.spring.boot"})
 public class Application {
 
     public static void main(String[] args){
@@ -12,4 +14,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
         System.out.println("main方法结束");
     }
+
 }
--- Java/spring-boot/1.spring boot 搭建web服务/spring-boot-web/src/main/java/com/glitter/spring/boot/web/DemoAction.java
+++ Java/spring-boot/1.spring boot 搭建web服务/spring-boot-web/src/main/java/com/glitter/spring/boot/web/DemoAction.java
@@ -2,10 +2,9 @@ package com.glitter.spring.boot.web;
 
 import com.glitter.spring.boot.bean.UserInfo;
 import com.glitter.spring.boot.common.ResponseResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/demo")
@@ -13,7 +12,32 @@ public class DemoAction{
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public ResponseResult add(@RequestBody UserInfo bean){
-        return ResponseResult.success(bean);
+        System.out.println(bean.toString());
+        return ResponseResult.success(null);
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public ResponseResult delete(@RequestParam Long id){
+        System.out.println(id);
+        return ResponseResult.success(null);
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+    public ResponseResult modify(@RequestBody UserInfo bean){
+        System.out.println(bean.toString());
+        return ResponseResult.success(null);
+    }
+
+    @RequestMapping(value = "/getUserInfo", method = RequestMethod.POST)
+    public ResponseResult<UserInfo> getUserInfo(@RequestParam Long id){
+        System.out.println();
+        return ResponseResult.success(null);
+    }
+
+    @RequestMapping(value = "/getUserInfos", method = RequestMethod.POST)
+    public ResponseResult<List<UserInfo>> getUserInfos(@RequestParam Long id){
+        System.out.println();
+        return ResponseResult.success(null);
     }
 
 }
