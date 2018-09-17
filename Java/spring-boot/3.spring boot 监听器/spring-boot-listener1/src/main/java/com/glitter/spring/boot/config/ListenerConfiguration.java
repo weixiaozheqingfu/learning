2e272ddb83ff54dc51538b990e6c9d4d31bf52b6
李孟军 <limengjun@syswin.com>
2018-09-17 11:06:35 +0800

    提交修改

--- Java/spring-boot/3.spring boot 监听器/spring-boot-listener1/src/main/java/com/glitter/spring/boot/config/ListenerConfiguration.java
+++ Java/spring-boot/3.spring boot 监听器/spring-boot-listener1/src/main/java/com/glitter/spring/boot/config/ListenerConfiguration.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 无论内置tomcat容器还是外置tomcat容器,都可以通过servletListenerRegistrationBean.setOrder()方法控制多个listener的加载和监听顺序.
+ * 推荐使用这种方式,而非使用注解方式。
+ */
 @Configuration
 public class ListenerConfiguration implements WebMvcConfigurer {
 
@@ -27,7 +31,7 @@ public class ListenerConfiguration implements WebMvcConfigurer {
         // 设置监听器
         servletListenerRegistrationBean.setListener(aemoServletContextListener());
         // 设置执行顺序(值越小,越先执行)
-        servletListenerRegistrationBean.setOrder(ServletListenerRegistrationBean.HIGHEST_PRECEDENCE);
+        servletListenerRegistrationBean.setOrder(ServletListenerRegistrationBean.HIGHEST_PRECEDENCE + 1);
         // 返回spring boot listener注册器对象
         return servletListenerRegistrationBean;
     }
@@ -39,7 +43,7 @@ public class ListenerConfiguration implements WebMvcConfigurer {
         // 设置监听器
         servletListenerRegistrationBean.setListener(demoServletContextListener());
         // 设置执行顺序(值越小,越先执行)
-        servletListenerRegistrationBean.setOrder(ServletListenerRegistrationBean.HIGHEST_PRECEDENCE + 1);
+        servletListenerRegistrationBean.setOrder(ServletListenerRegistrationBean.HIGHEST_PRECEDENCE);
         // 返回spring boot listener注册器对象
         return servletListenerRegistrationBean;
     }
