3115dd7d71560fdd32b7c0bcfab91b2cd30a3576
李孟军 <limengjun@syswin.com>
2018-09-19 11:40:53 +0800

    提交修改

--- Java/spring-boot/5.spring boot 全局异常/spring-boot-globalexception/src/main/java/com/glitter/spring/boot/exception/GlobalExceptionHandler.java
+++ Java/spring-boot/5.spring boot 全局异常/spring-boot-globalexception/src/main/java/com/glitter/spring/boot/exception/GlobalExceptionHandler.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * 使用@ControllerAdvice或@RestControllerAdvice处理全局异常有一定的局限性.只有进入Controller层的错误,才会由此全局异常类处理。
  * 拦截器抛出的异常,以及访问错误地址等情况,该全局异常类处理不了,由 Spring Boot默认的异常处理机制处理。
+ *
+ * 全局异常拦截主要用于不同的异常类,可以返回不同的数据到前端,具体方法内部如果没有特别需要就可以省略掉try catch了,直管抛出异常即可(不同异常返回值不同交给全局异常处理,异常日志记录交给aop日志来记录)。
+ * 至于异常日志的记录最好是aop日志进行记录,因为aop记录的日志可以很容易将方法的输入参数打印出来.
+ *
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
