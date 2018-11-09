7fd3c288edf9bcc813ba7cae4da25305c2cb1181
李孟军 <limengjun@syswin.com>
2018-11-09 16:50:14 +0800

    提交修改

--- Java/spring-boot/11.spring boot mybatis/spring-boot-mybatis5/src/main/java/com/glitter/spring/boot/Application.java
+++ Java/spring-boot/11.spring boot mybatis/spring-boot-mybatis5/src/main/java/com/glitter/spring/boot/Application.java
@@ -1,13 +1,11 @@
 package com.glitter.spring.boot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
