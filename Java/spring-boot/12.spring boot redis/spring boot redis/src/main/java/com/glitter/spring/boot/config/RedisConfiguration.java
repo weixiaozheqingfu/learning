2002a23626277699a29accb320c669141202eef2
李孟军 <limengjun@syswin.com>
2018-11-12 10:04:04 +0800

    提交修改

---
+++ Java/spring-boot/12.spring boot redis/spring boot redis/src/main/java/com/glitter/spring/boot/config/RedisConfiguration.java
@@ -0,0 +1,12 @@
+package com.glitter.spring.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class RedisConfiguration implements WebMvcConfigurer {
+
+
+
+}
