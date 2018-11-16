2ce9e311c44b33f6773798e5219b74b592063d39
李孟军 <limengjun@syswin.com>
2018-11-16 15:34:16 +0800

    提交

--- Java/spring-boot/13.spring boot login session/spring-boot-session/src/main/java/com/glitter/spring/boot/service/impl/Session.java
+++ Java/spring-boot/13.spring boot login session/spring-boot-session/src/main/java/com/glitter/spring/boot/service/impl/Session.java
@@ -105,6 +105,5 @@ public class Session implements ISession,Serializable {
         this.setAttributes(null);
         this.setCreationTime(null);
         this.setLastAccessedTime(null);
-
     }
 }
