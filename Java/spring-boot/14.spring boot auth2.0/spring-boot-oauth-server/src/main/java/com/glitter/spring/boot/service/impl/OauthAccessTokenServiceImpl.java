58c552c185a415804c2a1b1c29327db584c65ff1
李孟军 <limengjun@syswin.com>
2018-12-27 16:51:39 +0800

    提交

--- Java/spring-boot/14.spring boot auth2.0/spring-boot-oauth-server/src/main/java/com/glitter/spring/boot/service/impl/OauthAccessTokenServiceImpl.java
+++ Java/spring-boot/14.spring boot auth2.0/spring-boot-oauth-server/src/main/java/com/glitter/spring/boot/service/impl/OauthAccessTokenServiceImpl.java
@@ -21,34 +21,48 @@ public class OauthAccessTokenServiceImpl implements IOauthAccessTokenService{
     IOauthAccessTokenDao oauthAccessTokenDao;
 
     /**
-     * 创建accessToken表; InnoDB free: 488448 kB
-     * @param oauthAccessToken
+     * 验证accessToken,如果验证通过,则返回该accessToken对应的授权人openid,只有返回openid,本次的accessToken才是合法有效的
+     * <p>
+     * 只要accessToken是合法的,就能说明当前的请求是一个合法客户端的请求,并且本次具体请求的interfaceUri地址也在accessToken的授权范围内,一切都在红线内,
+     * 只是客户端请求的是服务器资源方,资源方是不知道当前的accessToken的对应的授权人信息的,需要将此信息返回给服务器资源方,告诉服务器资源方本次请求接口逻辑中只能处理该授权人相关的数据,数据不能越界.
+     * <p>
+     * (有的设计为了方便,在返回accessToken的时候同时返回了openid,然后调用资源方接口的时候,同时传入accessToken和openid,这种情况,资源法调用完validateAccessToken接口后
+     * 需要进一步根据validateAccessToken接口返回的openid与客户端传入的openid对比是否一致,一致才能访问接口,否则失败.
+     * 这种服务器资源方的接口设计也可以,他让客户端自己以参数的形式指明本次调用的接口要访问的数据范围即openid,然后服务器资源方接收到参数明白了当前客户端要访问这openid的数据,就看他的
+     * accessToken验证返回结果是不是这个openid,是的话说明没问题继续执行服务器接口逻辑并返回结果,反之,则客户端故意请求授权外的openid,企图蒙混过关,则服务器资源方进行异常抛出返回即可。
+     * )
+     *
+     * @param accessToken
+     * @param interfaceUri
+     * @return
      */
     @Override
-    public void create(OauthAccessToken oauthAccessToken) {
-        if(null == oauthAccessToken){
-            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS,"输入参数为空");
-        }
-        // TODO 参数校验
-        Date now = new Date();
-        oauthAccessToken.setCreateTime(now);
-        oauthAccessToken.setUpdateTime(now);
-        oauthAccessToken.setDeleteFlag(false);
-        oauthAccessTokenDao.insert(oauthAccessToken);
+    public Long validateAccessToken(String accessToken, String interfaceUri) {
+        // 1.验证accessToken是否存在
+
+        // 2.验证accessToken是否在有效期内
+
+        // 3.至此说明当前的发起请求的客户端可以访问openid的scope范围的接口数据,下面就看客户端本次发起的interfaceUri接口是否在scope授权范围内
+
+        // 4.验证当前请求接口interfaceUri是否在accessToken授权范围内
+
+        // 5.至此说明当前客户端发起了一个非常合法的请求,就是要访问授权人openid的interfaceUri接口
+
+        // 6.返回授权人openid,让服务器资源方知道当前接口的数据权限范围是局限在openid中的。
+
+        return null;
     }
 
+
     /**
-     * 修改accessToken表
-     * @param oauthAccessToken
+     * 根据openid获取系统中对应的userId
+     *
+     * @param openid
+     * @return
      */
     @Override
-    public void modifyById(OauthAccessToken oauthAccessToken) {
-        if(null == oauthAccessToken){
-            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS, "输入参数为空");
-        }
-        // TODO 参数校验
-        oauthAccessToken.setUpdateTime(new Date());
-        oauthAccessTokenDao.updateById(oauthAccessToken);
+    public Long getUserIdByOpenid(String openid) {
+        return null;
     }
 
     /**
