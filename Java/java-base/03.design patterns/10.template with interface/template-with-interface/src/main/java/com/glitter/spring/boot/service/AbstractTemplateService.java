fe8f41df852fdd1678e6876fe2eee42964cba5df
李孟军 <limengjun@syswin.com>
2019-07-19 10:19:26 +0800

    模板模式结合interface

---
+++ Java/java-base/03.design patterns/10.template with interface/template-with-interface/src/main/java/com/glitter/spring/boot/service/AbstractTemplateService.java
@@ -0,0 +1,67 @@
+package com.glitter.spring.boot.service;
+
+public abstract class AbstractTemplateService implements TemplateService{
+
+    /**
+     * 模板方法，定义算法骨架
+     */
+    @Override
+    public final void templateMethod(){
+        //第一步
+        this.operation1();
+        //第二步
+        this.operation2();
+        //第三步
+        this.doPrimitiveOperation1();
+        //第四步
+        this.doPrimitiveOperation2();
+        //第五步
+        this.hookOperation1();
+    }
+
+    /**
+     * 具体操作1，算法中的步骤，固定实现，而且子类不需要访问
+     */
+    private void operation1(){
+        //在这里具体的实现
+        System.out.println("模板父类私有方法operation1");
+    }
+
+    /**
+     * 具体操作2，算法中的步骤，固定实现，子类可能需要访问，
+     * 当然也可以定义成public的，不可以被覆盖，因此是final的
+     */
+    protected final void operation2(){
+        //在这里具体的实现
+        System.out.println("模板父类固定操作方法operation2");
+    }
+
+    /**
+     * 具体的AbstractClass操作，子类的公共功能，
+     * 但通常不是具体的算法步骤
+     */
+    protected void commonOperation(){
+        //在这里具体的实现
+        System.out.println("模板父类提供的公有操作方法commonOperation");
+    }
+
+    /**
+     * 原语操作1，算法中的必要步骤，父类无法确定如何真正实现，需要子类来实现
+     */
+    protected abstract void doPrimitiveOperation1();
+
+    /**
+     * 原语操作2，算法中的必要步骤，父类无法确定如何真正实现，需要子类来实现
+     */
+    protected abstract void doPrimitiveOperation2();
+
+    /**
+     * 钩子操作，算法中的步骤，不一定需要，提供缺省实现
+     * 由子类选择并具体实现
+     */
+    protected void hookOperation1(){
+        // 在这里提供缺省的实现
+        System.out.println("模板父类提供的钩子方法hookOperation1");
+    }
+
+}
---
+++ Java/java-base/03.design patterns/10.template with interface/template-with-interface/src/main/java/com/glitter/spring/boot/service/Main.java
@@ -0,0 +1,30 @@
+package com.glitter.spring.boot.service;
+
+import com.glitter.spring.boot.service.impl.Demo1ServiceImpl;
+import com.glitter.spring.boot.service.impl.Demo2ServiceImpl;
+
+
+/**
+ 模板模式和其他模式不冲突，并且可以和其他模式结合着用，尤其是和工厂模式。
+ 了解设计模式多了以后会发现，工厂模式就是基础设施一般的存在，很多模式都要结合着工厂模式使用才能发挥更好的效果。
+ 模板模式主要是提现了一种编排和约束的能力
+
+ 设计模式没有那么严格，有时候具体设计出来一种模式，你看起来可能会像多种设计模式，关键看你理解的侧重点是什么，不要拘泥于固定的东西，灵活运用即可。
+ */
+public class Main {
+
+    public static void main(String[] args){
+        test1();
+    }
+
+    private static void test1() {
+        TemplateService templateService1 = new Demo1ServiceImpl();
+        templateService1.templateMethod();
+
+        System.out.println("");
+
+        TemplateService templateService2 = new Demo2ServiceImpl();
+        templateService2.templateMethod();
+    }
+
+}
