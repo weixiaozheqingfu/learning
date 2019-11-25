8882f88c53307cd88b9475809fa7e32239d35059
limengjun <limengjun@syswin.com>
2019-11-25 16:05:03 +0800

    增加rabbitmq项目

---
+++ Java/spring-boot/18.spring boot rabbitmq/rabbitmq-producer/src/main/java/com/glitter/spring/boot/constant/GlitterConstants.java
@@ -0,0 +1,12 @@
+package com.glitter.spring.boot.constant;
+
+
+public class GlitterConstants {
+
+    public static final String SESSION_LOGIN_GRAPHCAPTCHA = "loginGraphCaptcha";
+
+    public static final String SESSION_USER = "userInfo";
+
+    public static final String JSESSIONID = "jsessionId";
+
+}
