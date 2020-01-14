fe752efe22dfadc4843d88a93fcd88aad3a74429
limengjun <limengjun@syswin.com>
2020-01-14 14:09:49 +0800

    增加mycat读写分离

---
+++ Java/spring-boot/11.spring boot mybatis/spring-boot-mybatis7/src/main/java/com/glitter/spring/boot/persistence/dao/IAuthServerInterfaceInfoDao.java
@@ -0,0 +1,95 @@
+package com.glitter.spring.boot.persistence.dao;
+
+import com.glitter.spring.boot.bean.AuthServerInterfaceInfo;
+import java.util.List;
+
+public interface IAuthServerInterfaceInfoDao{
+
+    /**
+    * 插入操作
+    *
+    * @param authServerInterfaceInfo
+    * @return
+    */
+    int insert(AuthServerInterfaceInfo authServerInterfaceInfo);
+
+    /**
+    * 根据主键删除
+    *
+    * @param id
+    * @return
+    */
+    int deleteById(Long id);
+
+    /**
+    * 根据主键集合删除
+    *
+    * @param ids
+    * @return
+    */
+    int deleteByIds(Long[] ids);
+
+    /**
+    * 根据条件删除
+    *
+    * @param authServerInterfaceInfo
+    * @return
+    */
+    int delete(AuthServerInterfaceInfo authServerInterfaceInfo);
+
+    /**
+    * 根据主键更新
+    *
+    * @param authServerInterfaceInfo
+    * @return
+    */
+    int updateById(AuthServerInterfaceInfo authServerInterfaceInfo);
+
+    /**
+     * 按主键查询
+     *
+     * @param id
+     * @return
+     */
+    AuthServerInterfaceInfo getById(Long id);
+
+    /**
+     * 按主键集合查询
+     *
+     * @param ids
+     * @return
+     */
+    List<AuthServerInterfaceInfo> getByIds(Long[] ids);
+
+    /**
+     * 按条件查询
+     *
+     * @param authServerInterfaceInfo
+     * @return
+     */
+    AuthServerInterfaceInfo get(AuthServerInterfaceInfo authServerInterfaceInfo);
+
+    /**
+     * 查询所有记录
+     *
+     * @return
+     */
+    List<AuthServerInterfaceInfo> findAllList();
+
+    /**
+     * 按条件查询
+     *
+     * @param authServerInterfaceInfo
+     * @return
+     */
+    List<AuthServerInterfaceInfo> findList(AuthServerInterfaceInfo authServerInterfaceInfo);
+
+    /**
+     * 按条件获取数量
+     *
+     * @param authServerInterfaceInfo
+     * @return
+     */
+    int getCount(AuthServerInterfaceInfo authServerInterfaceInfo);
+
+}
---
+++ Java/spring-boot/11.spring boot mybatis/spring-boot-mybatis7/src/main/java/com/glitter/spring/boot/service/impl/UserInfoServiceImpl.java
@@ -0,0 +1,122 @@
+package com.glitter.spring.boot.service.impl;
+
+
+import com.glitter.spring.boot.constant.CoreConstants;
+import com.glitter.spring.boot.exception.BusinessException;
+import com.glitter.spring.boot.service.IUserInfoService;
+import com.glitter.spring.boot.bean.UserInfo;
+import com.glitter.spring.boot.persistence.dao.IUserInfoDao;
+import com.pagehelper.plugin.PageHelper;
+import com.pagehelper.plugin.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserInfoServiceImpl implements IUserInfoService{
+
+    private static final Logger logger = LoggerFactory.getLogger(UserInfoServiceImpl.class);
+
+    @Autowired
+    IUserInfoDao userInfoDao;
+
+    /**
+     * 创建用户表; InnoDB free: 488448 kB
+     * @param userInfo
+     */
+    @Override
+    @Transactional
+    public void create(UserInfo userInfo) {
+        if(null == userInfo){
+            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS,"输入参数为空");
+        }
+        Date now = new Date();
+        userInfo.setCreateTime(now);
+        userInfo.setUpdateTime(now);
+        userInfo.setDeleteFlag(false);
+        userInfoDao.insert(userInfo);
+
+        if (userInfo.getAge()==1) {
+            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS,"故意报错");
+        }
+
+        UserInfo userInfo1 = new UserInfo();
+        userInfo1.setId(1L);
+        userInfo1.setAge(102);
+        userInfoDao.updateById(userInfo1);
+
+        UserInfo result1 = userInfoDao.getById(2L);
+        System.out.println("result1:" + result1.getAge());
+        UserInfo result2 = userInfoDao.getById(2L);
+        System.out.println("result2:" + result2.getAge());
+    }
+
+    /**
+     * 修改用户表; InnoDB free: 488448 kB
+     * @param userInfo
+     */
+    @Override
+    public void modifyById(UserInfo userInfo) {
+        if(null == userInfo){
+            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS, "输入参数为空");
+        }
+        // TODO 参数校验
+        userInfo.setUpdateTime(new Date());
+        userInfoDao.updateById(userInfo);
+    }
+
+    /**
+     * 根据主键删除用户表; InnoDB free: 488448 kB
+     * @param id
+     */
+    @Override
+    public void deleteById(Long id) {
+        if(null == id){
+            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS, "输入参数为空");
+        }
+        UserInfo record = new UserInfo();
+        record.setId(id);
+        record.setDeleteFlag(true);
+        record.setUpdateTime(new Date());
+        int count = userInfoDao.updateById(record);
+        if(count < 1){
+            logger.error("UserInfoServiceImpl.deleteById方法执行失败,输入参数:{}",id);
+            throw new BusinessException(CoreConstants.REQUEST_ERROR_PARAMS, "操作失败");
+        }
+    }
+
+    @Override
+    public PageInfo<UserInfo> getUserInfosPage(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<UserInfo> userInfos = userInfoDao.findAllList();
+        PageInfo<UserInfo> pageInfo = new PageInfo(userInfos);
+        return pageInfo;
+    }
+
+    /**
+     * 根据主键获取用户表; InnoDB free: 488448 kB
+     * @param id
+     * @return
+     */
+    @Override
+    // @Transactional(readOnly = true)
+    public UserInfo getUserInfoById(Long id) {
+        UserInfo result = null;
+        if(null == id){
+            return result;
+        }
+
+        UserInfo result1 = userInfoDao.getById(2L);
+        System.out.println("result1:" + result1.getAge());
+        UserInfo result2 = userInfoDao.getById(2L);
+        System.out.println("result2:" + result2.getAge());
+
+        return result1;
+    }
+
+}
